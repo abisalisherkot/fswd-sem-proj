47f3de4c8ce405d339b5e7476a024c42009e534c
abisalisherkot <abismehdi313@gmail.com>
2023-07-04 18:50:12 +0500

    Abis

--- server/db/conn.js
+++ server/db/conn.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const DB = process.env.DATABASE; //password/username now hidden
 
 mongoose.connect(DB).then(() => {
-    console.log('Connection Successfull!');
+    console.log('Connection Successfully!');
 }).catch((err) => {
     console.log(`No Connection`);
 })
